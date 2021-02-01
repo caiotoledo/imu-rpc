fb73bcb565c98d99457b2e958622524fee12c9eb
Caio Toledo <caioviniciusdetoledo@gmail.com>
2021-02-01 01:37:01 +0000

    Remove unused parameter in macro in LogInstance
    Signed-off-by: Caio Toledo <caioviniciusdetoledo@gmail.com>

--- lib/Logger/include/LogInstance.h
+++ lib/Logger/include/LogInstance.h
@@ -23,24 +23,24 @@ using LogSingleton = singleton::SingletonInstancer<logger::Log>;
 #define LOG_INSTANCE    (*LogSingleton::GetInstance())
 
 /* Standard Log macro */
-#define LOGMACRO(COLOR, LEVEL, ...)   do {\
+#define LOGMACRO(LEVEL, ...)   do {\
   LOG_INSTANCE.Print(logger::LEVEL, __FILENAME__, __FUNCTION__, __LINE__, __VA_ARGS__);\
 } while(0)
 
 #ifdef LEVEL_DEBUG
-#define LOGDEBUG(...)   LOGMACRO(GREEN, DEBUG, __VA_ARGS__)
+#define LOGDEBUG(...)   LOGMACRO(DEBUG, __VA_ARGS__)
 #else
 #define LOGDEBUG(...)
 #endif
 
 #ifdef LEVEL_WARNING
-#define LOGWARN(...)    LOGMACRO(YELLOW, WARNING, __VA_ARGS__)
+#define LOGWARN(...)    LOGMACRO(WARNING, __VA_ARGS__)
 #else
 #define LOGWARN(...)
 #endif
 
 #ifdef LEVEL_ERROR
-#define LOGERROR(...)   LOGMACRO(RED, ERROR, __VA_ARGS__)
+#define LOGERROR(...)   LOGMACRO(ERROR, __VA_ARGS__)
 #else
 #define LOGERROR(...)
 #endif
