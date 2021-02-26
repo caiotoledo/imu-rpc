56393c5bfcd8f4000382a8b970d27f03a6e93bbd
Caio Toledo <caioviniciusdetoledo@gmail.com>
2021-02-26 00:20:40 +0000

    Initial unittesting of IMUServer class
    Signed-off-by: Caio Toledo <caioviniciusdetoledo@gmail.com>

---
+++ server/IMUServer/tests/src/tests_imurpcserver.cpp
@@ -0,0 +1,69 @@
+#include <memory>
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include <MockIMUAbstraction.hpp>
+#include <MockIMUMath.hpp>
+#include <MockRPCServer.hpp>
+
+#include <IMURPCServer.hpp>
+
+using ::testing::_;
+using ::testing::Return;
+using ::testing::Invoke;
+using ::testing::SetArgReferee;
+using ::testing::DoAll;
+using ::testing::AtLeast;
+
+TEST(IMURPCServer, StartServer)
+{
+  /* Construct objects */
+  auto rpcMock = std::make_shared<RPCServer::MockRPCServer>();
+  auto imuMock = std::make_shared<IMUAbstraction::MockIMUAbstraction>();
+  auto mathMock = std::make_shared<IMUMath::MockIMUAbstraction>();
+
+  std::shared_ptr<IMUServer::IIMUServer> imuServer;
+  imuServer = std::make_shared<IMUServer::IMURPCServer>(rpcMock, imuMock, mathMock);
+
+  /* Prepare mock env */
+  EXPECT_CALL(*rpcMock, Init())
+    .Times(1)
+    .WillRepeatedly(Return(RPCServer::eRPCError::eRET_OK));
+
+  EXPECT_CALL(*rpcMock, setGetRawAccelCallback_rv(_))
+    .Times(1)
+    .WillRepeatedly(Return(RPCServer::eRPCError::eRET_OK));
+
+  EXPECT_CALL(*rpcMock, setGetRawGyroCallback_rv(_))
+    .Times(1)
+    .WillRepeatedly(Return(RPCServer::eRPCError::eRET_OK));
+
+  EXPECT_CALL(*rpcMock, setGetEulerAngleCallback_rv(_))
+    .Times(1)
+    .WillRepeatedly(Return(RPCServer::eRPCError::eRET_OK));
+
+  EXPECT_CALL(*rpcMock, setGetComplFilterAngleCallback_rv(_))
+    .Times(1)
+    .WillRepeatedly(Return(RPCServer::eRPCError::eRET_OK));
+
+  EXPECT_CALL(*rpcMock, DeInit())
+    .Times(1);
+
+  EXPECT_CALL(*imuMock, Init())
+    .Times(1)
+    .WillRepeatedly(Return(IMUAbstraction::eIMUAbstractionError::eRET_OK));
+
+  EXPECT_CALL(*imuMock, AddUpdateDataCallback_rv(_))
+    .Times(1);
+
+  EXPECT_CALL(*mathMock, Init())
+    .Times(1)
+    .WillRepeatedly(Return(IMUMath::eIMUMathError::eRET_OK));
+
+  /* Perform test */
+  auto ret = imuServer->StartServer();
+
+  /* Check Results */
+  EXPECT_EQ(ret, IMUServer::eIMUServerError::eRET_OK);
+}
