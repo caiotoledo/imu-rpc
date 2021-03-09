536780baf7fa2209a44c9f9c74461034e4ddeb8f
Caio Toledo <caioviniciusdetoledo@gmail.com>
2021-03-09 15:28:10 +0000

    SafeQueue: Async dequeue test
    Signed-off-by: Caio Toledo <caioviniciusdetoledo@gmail.com>

--- client/imu-socket/SafeQueue/tests/src/tests_safequeue.cpp
+++ client/imu-socket/SafeQueue/tests/src/tests_safequeue.cpp
@@ -1,3 +1,5 @@
+#include <future>
+
 #include <gtest/gtest.h>
 
 #include <SafeQueue.hpp>
@@ -33,4 +35,37 @@ TEST(SafeQueue, Empty_Queue)
 
   /* Check Results */
   EXPECT_EQ(ret, false);
+}
+
+TEST(SafeQueue, Async_Wait_Dequeue)
+{
+  /* Construct objects */
+  Queue::SafeQueue<int> queue;
+
+  /* Prepare local variables */
+  constexpr auto SLEEP_WAIT = 5; /* ms */
+  int expectedVal1 = 10, expectedVal2 = 20;
+  int val1 = 0, val2 = 0;
+
+  auto asyncDequeue = [&queue](int timeout)
+  {
+    int val = 0;
+    queue.dequeue(val, timeout);
+    return val;
+  };
+
+  /* Perform test */
+  auto fut1 = std::async(std::launch::async, asyncDequeue, -1); /* Wait forever for a value */
+  std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP_WAIT)); /* Keep async dequeue blocked waiting */
+  queue.enqueue(expectedVal1);
+  val1 = fut1.get();
+
+  auto fut2 = std::async(std::launch::async, asyncDequeue, 5*SLEEP_WAIT);
+  std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP_WAIT)); /* Keep async dequeue blocked waiting */
+  queue.enqueue(expectedVal2);
+  val2 = fut2.get();
+
+  /* Check Results */
+  EXPECT_EQ(val1, expectedVal1);
+  EXPECT_EQ(val2, expectedVal2);
 }
