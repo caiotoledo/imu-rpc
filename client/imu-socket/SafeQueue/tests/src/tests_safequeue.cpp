b906b428ad9ea40daaba982f50cbb6fc5cf0e518
Caio Toledo <caioviniciusdetoledo@gmail.com>
2021-03-09 14:55:35 +0000

    Initial Unit testing for SafeQueue
    Signed-off-by: Caio Toledo <caioviniciusdetoledo@gmail.com>

---
+++ client/imu-socket/SafeQueue/tests/src/tests_safequeue.cpp
@@ -0,0 +1,39 @@
+#include <memory>
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include <SafeQueue.hpp>
+
+TEST(SafeQueue, Simple_Enqueue_Dequeue)
+{
+  /* Construct objects */
+  Queue::SafeQueue<int> queue;
+
+  /* Prepare local variables */
+  int expectedVal = 10;
+  int val = 0;
+
+  /* Perform test */
+  queue.enqueue(expectedVal);
+  auto ret = queue.dequeue(val, -1);
+
+  /* Check Results */
+  EXPECT_EQ(ret, true);
+  EXPECT_EQ(val, expectedVal);
+}
+
+TEST(SafeQueue, Empty_Queue)
+{
+  /* Construct objects */
+  Queue::SafeQueue<int> queue;
+
+  /* Prepare local variables */
+  int val = 0;
+
+  /* Perform test */
+  auto ret = queue.dequeue(val, 1);
+
+  /* Check Results */
+  EXPECT_EQ(ret, false);
+}
