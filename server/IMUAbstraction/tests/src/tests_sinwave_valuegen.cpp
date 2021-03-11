fb1c8cfbe7b6d63e0b2ffeb32c44dc0f4c55b795
Caio Toledo <caioviniciusdetoledo@gmail.com>
2021-03-11 15:51:19 +0000

    Use Timer Mock class in sinwave ValueGen unittest
    Signed-off-by: Caio Toledo <caioviniciusdetoledo@gmail.com>

--- server/IMUAbstraction/tests/src/tests_sinwave_valuegen.cpp
+++ server/IMUAbstraction/tests/src/tests_sinwave_valuegen.cpp
@@ -5,7 +5,27 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
+#include <TimerInstance.hpp>
+
 #include <ValueGenImpl.hpp>
+
+using ::testing::Return;
+using ::testing::DoAll;
+using ::testing::AtLeast;
+
+#define DEG_TO_RAD(x)   (((double)x)*(M_PIl/(double)180.0))
+
+inline double round( double val )
+{
+  if( val < 0 ) return ceil(val - 0.5);
+  return floor(val + 0.5);
+}
+
+inline double sinVal(double amplitude, double freq, double timepoint, double phaseshift_rad)
+{
+  auto ret = amplitude * sin(2L * M_PIl * freq * timepoint + phaseshift_rad);
+  return ret;
+}
 
 /**
  * TESTING SIN WAVE GENERATOR
@@ -32,26 +52,36 @@ TEST_P(GetSinWaveValueTestsParameterized, GetSinWaveValue)
   valueGen = std::make_shared<IMUAbstraction::SinWaveValueGenerator>(freq,phaseshift);
 
   /* Prepare local variables */
-  constexpr auto TOLERANCE = 0.05;
+  double timestamp[] = {0,0,0};
   double valAccel[] = {0,0,0};
   double valGyro[] = {0,0,0};
+  const auto count_arr = sizeof(valAccel)/sizeof(valAccel[0]);
+
+  /* Prepare mock env */
+  EXPECT_CALL(TIMER_INSTANCE, GetSeconds())
+    .Times(3*(count_arr))
+    .WillRepeatedly(Return(10));
 
   /* Perform test */
-  for (size_t i = 0; i < sizeof(valAccel)/sizeof(valAccel[0]); i++)
+  for (size_t i = 0; i < count_arr; i++)
   {
     auto axis = static_cast<DBusTypes::eAxis>(i);
+    timestamp[i] = TIMER_INSTANCE.GetSeconds();
     valueGen->GetRawAccel(axis, valAccel[i], amp);
     valueGen->GetRawGyro(axis, valGyro[i], amp);
   }
 
   /* Check Results */
-  for (size_t i = 0; i < 2; i++)
+  for (size_t i = 0; i < count_arr; i++)
   {
-    auto gyro = abs(valGyro[i]);
-    auto accel = abs(valAccel[i+1]);
+    auto phaseshift_rad = DEG_TO_RAD(i*phaseshift);
+    auto expectedAccelVal = sinVal(amp, freq, timestamp[i], phaseshift_rad);
 
-    EXPECT_LE(gyro, accel*(1+TOLERANCE));
-    EXPECT_GE(gyro, accel*(1-TOLERANCE));
+    phaseshift_rad = DEG_TO_RAD((i+1)*phaseshift);
+    auto expectedGyroVal = sinVal(amp, freq, timestamp[i], phaseshift_rad);
+
+    EXPECT_EQ(round(expectedAccelVal), round(valAccel[i]));
+    EXPECT_EQ(round(expectedGyroVal), round(valGyro[i]));
   }
 }
 
@@ -60,10 +90,12 @@ INSTANTIATE_TEST_CASE_P(
     GetSinWaveValueTestsParameterized,
     ::testing::Values(
       /* Freq, PhaseShift, Amp */
-      std::make_tuple(0.1,45,1000),
+      std::make_tuple(0.5,90,1000), /* Value used in IMU-Daemon application */
+      std::make_tuple(10,45,10),
+      std::make_tuple(10,90,10),
+      std::make_tuple(1,45,100),
       std::make_tuple(0.1,90,1000),
-      std::make_tuple(0.05,90,1000),
-      std::make_tuple(0.01,45,1000),
-      std::make_tuple(0.01,45,10000)
+      std::make_tuple(0.05,45,1000),
+      std::make_tuple(0.01,90,1000)
     )
 );
